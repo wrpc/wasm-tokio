2ff9dbb3c6aad1cba299458fb8aef14fae8d3034
Roman Volosatovs <rvolosatovs@riseup.net>
2024-06-07 14:51:07 +0200

    chore: just export everything from subcrates
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 mod core;
-pub use core::{AsyncReadCore, AsyncWriteCore, CoreNameEncoder};
-pub use leb128_tokio::{AsyncReadLeb128, AsyncWriteLeb128, Leb128Encoder};
-pub use utf8_tokio::{AsyncReadUtf8, AsyncWriteUtf8, Utf8Codec};
 
 pub mod cm;
+
+pub use core::*;
+pub use leb128_tokio::*;
+pub use utf8_tokio::*;
 
 pub use tokio;
